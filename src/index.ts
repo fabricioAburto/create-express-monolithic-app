e42041496fe52f54fbef117b3a28bd20a20a7d47
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-07-11 19:39:16 +0200

    Adding templates

--- src/index.ts
+++ src/index.ts
@@ -20,49 +20,25 @@ export const questioner = async () => {
       message: 'Ingresa el nombre:',
     },
     {
-      type: 'list',
-      default: 'none',
-      name: 'templateEngine',
-      choices: ['hbs', 'html', 'none'],
-      message: 'Do you need a template engine:',
-    },
-    {
-      type: 'confirm',
-      default: false,
-      name: 'docker',
-      message: 'Do you need docker?',
-    },
-    {
-      type: 'list',
-      default: 'none',
-      name: 'database',
-      message: 'Do you need a database?',
-      choices: ['mongodb', 'mysql', 'none'],
+      name: 'port',
+      type: 'number',
+      default: 8080,
+      message: 'Ingresa el puero:',
     },
   ]);
   console.log(answers);
-};
-
-questioner().then(() => {});
-
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+  AppCreator.create(answers).catch(console.error);
 };
 
 export class AppCreator {
   static async create(configs: Configurations) {
     const profiler: Profiler = {
       NAME: configs.name,
-      DOCKER: configs.docker,
       PORT: configs.port || 3000,
-      DATABASE: configs.database,
-      TEMPLATE_ENGINE: configs.templateEngine,
     };
 
     await ncp(path.join(__dirname, `../templates/express`), configs.name);
+
     AppCreator.renameFile(configs.name, 'gitignore', '.gitignore');
 
     glob.sync(`${configs.name}/**/*`).forEach(file => {
@@ -70,12 +46,6 @@ export class AppCreator {
     });
   }
 
-  /**
-   * Rename a given file
-   * @param filename
-   * @param oldName
-   * @param newName
-   */
   static renameFile(filename: string, oldName: string, newName: string) {
     if (fs.existsSync(path.normalize(`${filename}/${oldName}`))) {
       fs.renameSync(
@@ -84,11 +54,7 @@ export class AppCreator {
       );
     }
   }
-  /**
-   * Add values to the files
-   * @param filename
-   * @param profiler
-   */
+
   static template(filename: string, profiler: Profiler) {
     const content = fs.readFileSync(filename, 'utf8').toString();
     const template = Object.entries(profiler).reduce((acc, [key, value]) => {
@@ -100,3 +66,5 @@ export class AppCreator {
     fs.writeFileSync(filename, template);
   }
 }
+
+questioner().then(() => {});
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,9 @@
 export type Configurations = {
   name: string;
   port?: number;
-  docker?: boolean;
-  templateEngine?: 'hbs' | 'html' | 'none';
-  database?: 'mysql' | 'mongodb';
 };
 
 export type Profiler = {
   NAME: string;
   PORT?: number;
-  DOCKER?: boolean;
-  TEMPLATE_ENGINE?: 'hbs' | 'html' | 'none';
-  DATABASE?: 'mysql' | 'mongodb' | 'none';
 };
